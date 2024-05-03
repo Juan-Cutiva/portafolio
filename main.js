5e9ae334128f168bebfdda0cadb54fb2ef0a82d3
Juan David Cutiva <69434726+Juan-Cutiva@users.noreply.github.com>
2024-05-03 11:34:58 -0500

    Ci

--- main.js
+++ main.js
@@ -1,2 +1,11 @@
+const fulImgBox = document.getElementById("fulImgBox"),
+fulImg = document.getElementById("fulImg")
 
+function openFulImg(){
+    fulImgBox.style.display = "flex";
+    fulImg.src = this.src;
+}
 
+function closeImg(){
+    fulImgBox.style.display = "none";
+}
